bf70785d36e96f70c33102d17d178518bcff65ce
falcon2017 <libyhurri@outlook.com>
2016-10-15 22:27:48 +0300

    Update CaptchaRefresh.meta.js

--- CaptchaRefresh.meta.js
+++ CaptchaRefresh.meta.js
@@ -3,8 +3,8 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @namespace   http://reservation.nid.gov.ly/#/Reserve
 // @include     http://*.nid.gov.ly/*
-// @downloadURL http://localhost:81/CaptchaRefresh.user.js
-// @updateURL   http://localhost:81/CaptchaRefresh.meta.js
+// @downloadURL https://github.com/falcon2017/reserve/blob/master/CaptchaRefresh.user.js
+// @updateURL   https://github.com/falcon2017/reserve/blob/master/CaptchaRefresh.meta.js
 // @version     1
 // @grant       none
 // ==/UserScript==
