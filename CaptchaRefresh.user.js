72c15950072c5725fc5fefeacdbb36d8cc49b310
falcon2017 <libyhurri@outlook.com>
2016-10-15 22:24:47 +0300

    Add files via upload

---
+++ CaptchaRefresh.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        CaptchaRefresh
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @namespace   http://reservation.nid.gov.ly/#/Reserve
+// @include     http://*.nid.gov.ly/*
+// @downloadURL http://localhost:81/CaptchaRefresh.user.js
+// @updateURL   http://localhost:81/CaptchaRefresh.meta.js
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+
+
+$('#_PageHeader__Logo').click(function(){ 
+    alert("sixth");
+});
